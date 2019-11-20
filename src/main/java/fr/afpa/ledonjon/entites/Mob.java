f03f201d23838af971abb9d41afaae22a6a9badb
Stephane <garciastephan@hotmail.com>
2019-11-20 11:34:40 +0100

    SG_creation_class_Mob

---
+++ src/main/java/fr/afpa/ledonjon/entites/Mob.java
@@ -0,0 +1,9 @@
+package fr.afpa.ledonjon.entites;
+
+public class Mob extends Character{
+	
+	public Mob(int healthPoint, int strength, int gold, boolean alive) {
+		super(healthPoint, strength, gold, alive);
+	}
+
+}
