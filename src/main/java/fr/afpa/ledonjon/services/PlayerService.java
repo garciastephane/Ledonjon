dd29c42ee65c45fcf318d0d2460291517927597f
David Didolla <daviddidolla@gmail.com>
2019-11-20 15:57:47 +0100

    DD_heal

--- src/main/java/fr/afpa/ledonjon/services/PlayerService.java
+++ src/main/java/fr/afpa/ledonjon/services/PlayerService.java
@@ -7,5 +7,8 @@ public class PlayerService {
 	public static Player CreatePlayer(Start start) {
 		return new Player(9 + UtilService.RamdomNumberGenerator(5), 2 + UtilService.RamdomNumberGenerator(2), 0, true, start);
 	}
-
+	
+	public static void Heal(Player didier) {
+		didier.setHealthPoint(didier.getHealthPoint()+1+ UtilService.RamdomNumberGenerator(2));
+	}
 }
