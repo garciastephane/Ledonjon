269eeb0be4cdc27f861aad99b886b1c6b805f5db
David Didolla <daviddidolla@gmail.com>
2019-11-20 11:21:47 +0100

    DD_class_Player

--- src/main/java/fr/afpa/ledonjon/entites/Character.java
+++ src/main/java/fr/afpa/ledonjon/entites/Character.java
@@ -1,5 +1,57 @@
 package fr.afpa.ledonjon.entites;
 
 public abstract class Character {
+	protected int heathPoint;
+	protected int strength;
+	protected int gold;
+	protected boolean alive;
+	
+	/**
+	 * Construtor
+	 * @param heathPoint
+	 * @param strength
+	 * @param gold
+	 * @param alive
+	 */
+	public Character(int heathPoint, int strength, int gold, boolean alive) {
+		super();
+		this.heathPoint = heathPoint;
+		this.strength = strength;
+		this.gold = gold;
+		this.alive = alive;
+	}
 
+	public int getHeathPoint() {
+		return heathPoint;
+	}
+
+	public void setHeathPoint(int heathPoint) {
+		this.heathPoint = heathPoint;
+	}
+
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	public int getGold() {
+		return gold;
+	}
+
+	public void setGold(int gold) {
+		this.gold = gold;
+	}
+
+	public boolean isAlive() {
+		return alive;
+	}
+
+	public void setAlive(boolean alive) {
+		this.alive = alive;
+	}
+	
+	
 }
---
+++ src/main/java/fr/afpa/ledonjon/entites/Player.java
@@ -0,0 +1,9 @@
+package fr.afpa.ledonjon.entites;
+
+public class Player extends Character{
+
+	public Player(int heathPoint, int strength, int gold, boolean alive) {
+		super(heathPoint, strength, gold, alive);
+	}
+
+}
