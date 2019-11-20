3e933958aa6378ddb871e1727c9aeaea3faf6249
David Didolla <daviddidolla@gmail.com>
2019-11-20 14:12:23 +0100

    DD_correction

--- src/main/java/fr/afpa/ledonjon/services/PlayerService.java
+++ src/main/java/fr/afpa/ledonjon/services/PlayerService.java
@@ -3,8 +3,9 @@ package fr.afpa.ledonjon.services;
 import fr.afpa.ledonjon.entites.Player;
 import fr.afpa.ledonjon.entites.Start;
 
-public class ServicePlayer {
+public class PlayerService {
 	public static Player CreatePlayer(Start start) {
+		
 		return new Player(9 + UtilService.RamdomNumberGenerator(5), 2 + UtilService.RamdomNumberGenerator(2), 0, true, start);
 	}
 }
