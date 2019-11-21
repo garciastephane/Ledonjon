392f827b99ec25673ed9c38085596ddba9a7507a
Stephane <garciastephan@hotmail.com>
2019-11-21 15:29:49 +0100

    SG_Create_Item

--- src/main/java/fr/afpa/ledonjon/services/ItemService.java
+++ src/main/java/fr/afpa/ledonjon/services/ItemService.java
@@ -1,9 +1,48 @@
 package fr.afpa.ledonjon.services;
 
+
+import fr.afpa.ledonjon.entites.GoldPouch;
+import fr.afpa.ledonjon.entites.HealthPotion;
 import fr.afpa.ledonjon.entites.Player;
+import fr.afpa.ledonjon.entites.Room;
+import fr.afpa.ledonjon.entites.SlotMachine;
+import fr.afpa.ledonjon.entites.StrengthPotion;
 import fr.afpa.ledonjon.ihm.ItemIhm;
 
 public class ItemService {
+	
+	/**
+	 * Methode pour creer une potion de vie
+	 * @param room
+	 */
+	public static void CreateHealthPotion(Room room) {
+		room.getItems().add(new HealthPotion());	
+	}
+	
+	/**
+	 * Methode pour creer une potion de force
+	 * @param room
+	 */
+	public static void CreateStrengthPotion(Room room) {
+		room.getItems().add(new StrengthPotion());
+	}
+	/**
+	 *  Methode pour creer une poche d or
+	 * @param room
+	 */
+	
+	public static void CreateGoldPouch(Room room) {
+		room.getItems().add(new GoldPouch());
+	}
+	
+	/**
+		 *  Methode pour creer une machine a sous
+		 */
+	public static void CreateSlotMachine(Room room) {
+		room.getItems().add(new SlotMachine());
+		
+		
+	}
 	public static void Heal(Player didier) {
 		int receavedHp = UtilService.RamdomNumberGenerator(2) + 1;
 		PlayerService.GainHp(didier, receavedHp);
@@ -31,20 +70,36 @@ public class ItemService {
 		} else if (d100 > 50 && d100 <= 55) {
 			GainStrength(didier);
 		} else if (d100 > 55 && d100 <= 100) {
-			if (d100 > 60 && d100 <= 65) {
-				CharacterSevice.LoseHealth(didier, 1);
+			if (d100 > 55 && d100 <= 60) {
+
+			} else if (d100 > 60 && d100 <= 65) {
+
+			} else if (d100 > 60 && d100 <= 65) {
+
+			} else if (d100 > 65 && d100 <= 65) {
+
+			} else if (d100 > 70 && d100 <= 76) {
+
 			} else if (d100 == 77) {
-				PlayerService.WinGold(didier, 1000);
+
+			} else if (d100 > 78 && d100 <= 80) {
+
+			} else if (d100 > 80 && d100 <= 85) {
+
+			} else if (d100 > 85 && d100 <= 95) {
+
 			} else if (d100 > 95 && d100 <= 97) {
-				PlayerService.GainHp(didier, 5);
+
 			} else if (d100 == 98) {
-				CharacterSevice.LoseHealth(didier, didier.getHealthPoint()-1);
+
 			} else if (d100 == 99) {
-				CharacterSevice.LoseHealth(didier, didier.getHealthPoint());
+
 			} else if (d100 == 100) {
-				//useItem(new SlotMachine());
+
 			}
 			ItemIhm.RewardDisplay(d100);
 		}
 	}
+
+	
 }
