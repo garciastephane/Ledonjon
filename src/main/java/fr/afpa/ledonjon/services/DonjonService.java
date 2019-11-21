eb2312159b59e398883abf6bbf458104b7a54a36
Stephane <garciastephan@hotmail.com>
2019-11-21 15:07:04 +0100

    SG_generate_Item

--- src/main/java/fr/afpa/ledonjon/services/DonjonService.java
+++ src/main/java/fr/afpa/ledonjon/services/DonjonService.java
@@ -1,6 +1,8 @@
 package fr.afpa.ledonjon.services;
 
+import fr.afpa.ledonjon.entites.Coordonate;
 import fr.afpa.ledonjon.entites.Donjon;
+import fr.afpa.ledonjon.entites.Room;
 
 public class DonjonService {
 
@@ -9,8 +11,34 @@ public class DonjonService {
 		int numMob = UtilService.RamdomNumberGenerator(7) + 1;
 
 		for (int i = 0; i < numMob; i++) {
+
 			donjon.getMobs().add(MobService.CreateMob(donjon.getMap()));
 		}
 		return true;
+	}
+
+	public static void generateItem(Coordonate map) {
+		int numItem = UtilService.RamdomNumberGenerator(7) + 1;
+
+		for (int i = 0; i < numItem; i++) {
+			int typeItem = UtilService.RamdomNumberGenerator(4);
+			Room room = RoomServices.FindValidRoom(map);
+			switch (typeItem) {
+			case 0:
+				ItemService.CreateHealthPotion(room);
+				break;
+			case 1:
+				ItemService.CreateStrengthPotion(room);
+				break;
+			case 2:
+				ItemService.CreateGoldPouch(room);
+				break;
+			case 3:
+				ItemService.CreateSlotMachine(room);
+				break;
+			default:
+				break;
+			}
+		}
 	}
 }
