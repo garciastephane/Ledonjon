19b3dc475270e188684827e5bc8b96c1af046bea
Stephane <garciastephan@hotmail.com>
2019-11-21 09:45:25 +0100

    SG_generate_Mob

--- src/main/java/fr/afpa/ledonjon/services/DonjonService.java
+++ src/main/java/fr/afpa/ledonjon/services/DonjonService.java
@@ -1,13 +1,16 @@
 package fr.afpa.ledonjon.services;
 
-import fr.afpa.ledonjon.entites.Mob;
+import fr.afpa.ledonjon.entites.Donjon;
 
 public class DonjonService {
 
-	public Mob generateMob() {
-		
-		
-		
-		return null;
+	public static boolean generateMob(Donjon donjon) {
+
+		int numMob = UtilService.RamdomNumberGenerator(7) + 1;
+
+		for (int i = 0; i < numMob; i++) {
+			donjon.getMobs().add(MobService.CreateMob(donjon.getMap()));
+		}
+		return true;
 	}
 }
