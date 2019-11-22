a66717519a57d394b812c85aad3d83ab956ff072
David Didolla <daviddidolla@gmail.com>
2019-11-22 15:20:19 +0100

    DD_displayTitle

--- src/main/java/fr/afpa/ledonjon/Gameplay.java
+++ src/main/java/fr/afpa/ledonjon/Gameplay.java
@@ -2,6 +2,7 @@ package fr.afpa.ledonjon;
 
 import fr.afpa.ledonjon.entites.Donjon;
 import fr.afpa.ledonjon.ihm.DonjonIhm;
+import fr.afpa.ledonjon.ihm.MenuIhm;
 import fr.afpa.ledonjon.services.DonjonService;
 import fr.afpa.ledonjon.services.PlayerService;
 
@@ -13,17 +14,22 @@ public class Gameplay {
 	public static void main(String[] args) {
 //		Donjon donjon = new Donjon(DonjonService.configXMaze(), DonjonService.configYMaze());
 //		DonjonService.generateMaze(donjon, donjon.getX()/ 2, donjon.getY()/ 2);
-		int tailleX = 5;
-		int tailleY = 5;
+		int tailleX = 10;
+		int tailleY = 10;
 		Donjon donjon = new Donjon(tailleX, tailleY);
 		DonjonService.generateMaze(donjon, tailleX / 2, tailleY / 2);
-		DonjonIhm.DisplayDonjon(donjon);
 		PlayerService.CreatePlayer(donjon.getMaze()[0][0]);
 		DonjonService.generateMob(donjon, tailleX ,tailleY);
 		DonjonService.generateItem(donjon, tailleX ,tailleY);
+		MenuIhm.DisplayTitle();
 		DonjonIhm.DisplayPlayerInDonjon(donjon);
-		DonjonIhm.DisplayFullDonjon(donjon);
 		System.out.println("oui");
+		boolean win = false;
+		do {
+			
+			win = true;
+		} while (!win);
+		
 	}
 
 }
--- src/main/java/fr/afpa/ledonjon/ihm/MenuIhm.java
+++ src/main/java/fr/afpa/ledonjon/ihm/MenuIhm.java
@@ -3,7 +3,6 @@ package fr.afpa.ledonjon.ihm;
 import fr.afpa.ledonjon.controles.RoomControl;
 import fr.afpa.ledonjon.entites.Room;
 
-
 public class MenuIhm {
 
 	/**
@@ -26,6 +25,16 @@ public class MenuIhm {
 			System.out.println("Move West");
 			System.out.println("Move South");
 		}
+	}
+
+	public static void DisplayTitle() {
+		System.out.println("" + "THE SACRED LAIR OF THE ANCIENT ELDER MAGIC SORCEROR DRAGON LORD OF THE ULTIMATE \r\n"
+				+ "         STRONGHOLD OF THE NORTHERN LAND OF THE INFINITE SOUTHEN COST,\r\n"
+				+ "LOYAL VASAL OF THE MAGNIFICENT ROYAL EMPEROR OF THE EIGHTEEN CONQUERED KINGDOMS \r\n"
+				+ " AND THE HIGH CASTLE OF THE GREATEST EMPIRE THE FORBIDEN PLANETE OF THE HIGHER \r\n"
+				+ "           DEVILISH GALAXIE OF THE BRIGTEST RISING LOST LAST STAR,\r\n"
+				+ "             POWERFUL GUADIAN OF THE GREAT AND STRONG DONJON OF \r\n"
+				+ "             THE MISTIC HEROIC SECRET LEGEND OF THE FINAL QUEST \r\n\n");
 	}
 
 }
